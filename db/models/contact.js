4603d66b2c732fbc219f0a52ce3f770b50003d49
Claire Ouellette <claireelisey@gmail.com>
2018-10-04 21:31:54 -0500

    Assignment 3 Creating Contacts and Connecting Databases: Completed

--- db/models/contact.js
+++ db/models/contact.js
@@ -1,8 +1,10 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   var Contact = sequelize.define('Contact', {
     name: DataTypes.STRING,
-    phone: DataTypes.STRING
+    phone: DataTypes.STRING,
+    email: DataTypes.STRING
   }, {});
   Contact.associate = function(models) {
     // associations can be defined here
